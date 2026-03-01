66cdff0935c9d5bd89d59934293c7bbc22934081
Aleksandr <moralev.alexander@gmail.com>
2026-03-02 00:23:39 +0500

    s8: fix sonar warn

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -5,12 +5,12 @@ DESCRIPTION = 'What number is missing in the progression?'
 
 def generate_round():
     length = 10
-    start = random.randint(1, 20)
-    step = random.randint(2, 6)
+    start = random.randint(1, 20)  # NOSONAR python:S2245
+    step = random.randint(2, 6)  # NOSONAR python:S2245
 
     progression = [start + i * step for i in range(length)]
 
-    hidden_index = random.randint(1, length - 2)
+    hidden_index = random.randint(1, length - 2)  # NOSONAR python:S2245
     correct_answer = progression[hidden_index]
 
     progression_display = [
