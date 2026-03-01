6a95d6e2d6ac0e4b812c4d0b2e02cfac8985e243
Aleksandr <moralev.alexander@gmail.com>
2026-03-02 01:05:17 +0500

    s9: brain-prime

---
+++ brain_games/games/prime.py
@@ -0,0 +1,25 @@
+import random
+
+DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+
+
+def is_prime(n: int) -> bool:
+    if n <= 1:
+        return False
+    if n <= 3:
+        return True
+    if n % 2 == 0 or n % 3 == 0:
+        return False
+    i = 5
+    while i * i <= n:
+        if n % i == 0 or n % (i + 2) == 0:
+            return False
+        i += 6
+    return True
+
+
+def generate_round():
+    number = random.randint(2, 100)  # NOSONAR python:S2245
+    question = str(number)
+    correct_answer = 'yes' if is_prime(number) else 'no'
+    return question, correct_answer
